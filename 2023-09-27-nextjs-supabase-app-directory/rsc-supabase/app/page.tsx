bcc209c81d75ffc6fa17e0fdabc500ab0d00f283
phasetr <phasetr@gmail.com>
2023-09-27 17:02:56 +0900

    Section2, Lesson12

--- 2023-09-27-nextjs-supabase-app-directory/rsc-supabase/app/page.tsx
+++ 2023-09-27-nextjs-supabase-app-directory/rsc-supabase/app/page.tsx
@@ -1,11 +1,19 @@
 import NotesList from "@/app/components/notes-list";
+import TimerCounter from "@/app/components/timer-counter";
+import { Suspense } from "react";
+import Spinner from "@/app/components/spinner";
+import RefreshBtn from "@/app/components/refresh-btn";
 
 export default function Pate() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World.</p>
-        <NotesList/>
+        <Suspense fallback={<Spinner color="border-green-500"/>}>
+          <NotesList/>
+        </Suspense>
+        <TimerCounter/>
+        <RefreshBtn/>
       </div>
     </main>
   );
