ca259719faeea6641dc5916c3ee32c8cb8dccc34
phasetr <phasetr@gmail.com>
2023-09-27 16:41:04 +0900

    Section2, Lesson11

--- 2023-09-27-nextjs-supabase-app-directory/rsc-supabase/app/page.tsx
+++ 2023-09-27-nextjs-supabase-app-directory/rsc-supabase/app/page.tsx
@@ -1,7 +1,12 @@
+import NotesList from "@/app/components/notes-list";
+
 export default function Pate() {
   return (
     <main>
-      <div className="m-10 text-center">Hello World.</div>
+      <div className="m-10 text-center">
+        <p>Hello World.</p>
+        <NotesList/>
+      </div>
     </main>
   );
 }
