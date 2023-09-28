eb31364b596f9d104276f3aa437b3510fefffd1d
phasetr <phasetr@gmail.com>
2023-09-28 17:07:04 +0900

    section3-15

--- 2023-09-28-fullstack-sns-development/api/server.js
+++ 2023-09-28-fullstack-sns-development/api/server.js
@@ -13,10 +13,26 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
+// 新規ユーザー登録API
 app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await prisma.user.create({ data: { username, email, password: hashedPassword } });
   return res.json(user);
 })
+
+// ログインAPI
+app.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(401).json({ error: "User not found" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "Invalid password" });
+  }
+})
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
