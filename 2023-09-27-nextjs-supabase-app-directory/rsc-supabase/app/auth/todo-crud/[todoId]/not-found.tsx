6e99e4fe30a16f5e1c3ea259ab12b3aae9173e82
phasetr <phasetr@gmail.com>
2023-09-28 14:36:55 +0900

    Section2, Lesson17

---
+++ 2023-09-27-nextjs-supabase-app-directory/rsc-supabase/app/auth/todo-crud/[todoId]/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div className="my-6">Task Details Not Found</div>;
+}
---
+++ 2023-09-27-nextjs-supabase-app-directory/rsc-supabase/app/components/todo-list.tsx
@@ -0,0 +1,17 @@
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "../../../database.types";
+import { cookies, headers } from "next/headers";
+import TodoItem from "@/app/components/todo-item";
+
+export default async function TodoList() {
+  const supabase = createServerComponentSupabaseClient<Database>({ headers, cookies });
+  const { data: todos } = await supabase
+    .from("todos")
+    .select("*")
+    .order("created_at", { ascending: true });
+  return (
+    <ul className="my-6 mx-3">
+      {todos?.map((todo) => <TodoItem key={todo.id} {...todo}/>)}
+    </ul>
+  );
+}
