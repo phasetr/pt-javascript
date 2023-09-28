e55e39bcc6fe2a8fc50836dde71943340783371c
phasetr <phasetr@gmail.com>
2023-09-28 22:15:19 +0900

    section4-21

--- 2023-09-28-fullstack-sns-development/client/app/page.tsx
+++ 2023-09-28-fullstack-sns-development/client/app/page.tsx
@@ -1,7 +1,9 @@
+import Timeline from "@/app/components/timeline";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold text-center">Hello World!</h1>
-    </main>
+    <div>
+      <Timeline/>
+    </div>
   );
 }
