029f6aba5ae341c1385c48ac04da28c86e829f6b
phasetr <phasetr@gmail.com>
2023-09-28 18:08:45 +0900

    section3-18

--- 2023-09-28-fullstack-sns-development/api/server.js
+++ 2023-09-28-fullstack-sns-development/api/server.js
@@ -1,43 +1,11 @@
 const express = require("express");
-require("dotenv").config();
-const bcrypt = require("bcrypt");
-const { PrismaClient } = require("@prisma/client");
-const jwt = require("jsonwebtoken");
-
 const app = express();
+const authRouter = require("./routers/auth");
+
+require("dotenv").config();
 const PORT = 8000;
-const prisma = new PrismaClient();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  console.log("Hello World!");
-  res.send("<h1>Hello World!</h1>");
-});
-
-// 新規ユーザー登録API
-app.post("/api/auth/register", async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await prisma.user.create({ data: { username, email, password: hashedPassword } });
-  return res.json(user);
-})
-
-// ログインAPI
-app.post("/api/auth/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) {
-    return res.status(401).json({ error: "User not found" });
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    return res.status(401).json({ error: "Invalid password" });
-  }
-
-  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: "1d" });
-  return res.json({ token });
-})
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
