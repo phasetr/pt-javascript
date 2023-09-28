71ff932e771054d354b157271bfac9ba179bf2f9
phasetr <phasetr@gmail.com>
2023-09-28 16:58:06 +0900

    section3-13

--- 2023-09-28-fullstack-sns-development/api/server.js
+++ 2023-09-28-fullstack-sns-development/api/server.js
@@ -1,18 +1,20 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const PORT = 8000;
 
 const prisma = new PrismaClient();
-
 app.get("/", (req, res) => {
   console.log("Hello World!");
   res.send("<h1>Hello World!</h1>");
 });
 
-app.post("/api/auth/register", async(req, res) => {
+app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
-  const user = await prisma.user.create({ data: { username, email, password } });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({ data: { username, email, password: hashedPassword } });
   return res.json(user);
 })
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
