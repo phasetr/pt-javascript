51370cae8faa66de32401f132127aa204c11be70
phasetr <phasetr@gmail.com>
2023-09-28 22:05:18 +0900

    section4-20

---
+++ 2023-09-28-fullstack-sns-development/client/app/components/nav-bar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <header className="bg-gray-700 p-4 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="font-semibold text-xl">
+          <Link href="/" className="text-2xl font-medium">SNS Clone</Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <>
+              <Link
+                href="/login"
+                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+              >
+                Login
+              </Link>
+              <Link
+                href="/signup"
+                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+              >
+                Sign Up
+              </Link>
+            </>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
