3852a9b24f839b1b5b5a3c974395dd2719c97298
phasetr <phasetr@gmail.com>
2023-09-29 23:06:23 +0900

    section4-57

--- 2023-09-28-fullstack-sns-development/client/app/profile/[userId]/page.tsx
+++ 2023-09-28-fullstack-sns-development/client/app/profile/[userId]/page.tsx
@@ -1,22 +1,27 @@
 import apiClient from "@/app/lib/api-client";
+import { PostType, ProfileType } from "@/types";
 
 type PageProps = { params: { userId: string } }
 
 export default async function UserProfile({ params }: PageProps) {
   const { userId } = params;
 
-  async function getProfile() {
+  async function getData() {
     try {
       const profileResponse = await apiClient.get(`/users/profile/${userId}`);
-      console.log(profileResponse);
-      return profileResponse.data;
+      const postsResponse = await apiClient.get(`/posts/${userId}`);
+
+      return {
+        profile: profileResponse.data as ProfileType,
+        posts: postsResponse.data as PostType[]
+      };
     } catch (e) {
       console.error(e);
       return { notFound: true };
     }
   }
 
-  const profile = await getProfile();
+  const { profile, posts } = await getData();
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -30,18 +35,20 @@ export default async function UserProfile({ params }: PageProps) {
             </div>
           </div>
         </div>
-        <div className="bg-white shadow-md rounded p-4 mb-4">
-          <div className="mb-4">
-            <div className="flex items-center mb-2">
-              <img className="w-10 h-10 rounded-full mr-2" alt="User Avatar"/>
-              <div>
-                <h2 className="font-semibold text-md">shincode</h2>
-                <p className="text-gray-500 text-sm">2023/05/08</p>
+        {posts?.map((post) => (
+          <div className="bg-white shadow-md rounded p-4 mb-4" key={post.id}>
+            <div className="mb-4">
+              <div className="flex items-center mb-2">
+                <img className="w-10 h-10 rounded-full mr-2" alt="User Avatar" src={profile.profileImageUrl}/>
+                <div>
+                  <h2 className="font-semibold text-md">{post.author.username}</h2>
+                  <p className="text-gray-500 text-sm">{new Date(post.createdAt).toLocaleString()}</p>
+                </div>
               </div>
+              <p className="text-gray-700">{post.content}</p>
             </div>
-            <p className="text-gray-700">はじめての投稿です。</p>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
