2bf0fdab9fad85df365ac4fd93dfee2fbd602020
phasetr <phasetr@gmail.com>
2023-09-29 17:58:21 +0900

    section4-53

--- 2023-09-28-fullstack-sns-development/api/routers/users.js
+++ 2023-09-28-fullstack-sns-development/api/routers/users.js
@@ -3,7 +3,7 @@ const { PrismaClient } = require("@prisma/client");
 const isAuthenticated = require("../middlewares/isAuthenticated");
 const prisma = new PrismaClient();
 
-
+// ユーザー情報取得用API
 router.get("/find", isAuthenticated, async (req, res) => {
   try {
     const user = await prisma.user.findUnique({
@@ -14,6 +14,29 @@ router.get("/find", isAuthenticated, async (req, res) => {
     }
 
     return res.status(200).json({ user: { id: user.id, username: user.username, email: user.email } });
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: "Internal server error" });
+  }
+})
+
+router.get("/profile/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    // TODO: パスワードなど余計な情報を削る
+    const profile = await prisma.profile.findUnique({
+      where: { userId: parseInt(userId) },
+      include: {
+        user: {
+          include: { profile: true }
+        }
+      }
+    });
+    if (!profile) {
+      res.status(404).json({ message: "Profile not found" });
+    }
+
+    return res.status(200).json(profile);
   } catch (err) {
     console.log(err)
     return res.status(500).json({ message: "Internal server error" });
