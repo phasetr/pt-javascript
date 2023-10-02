b062d400b7fc2a3f4640c3168a89034dc5351c6e
phasetr <phasetr@gmail.com>
2023-10-02 20:32:34 +0900

    This code passes a test.

---
+++ 2023-10-02-udemy-NEXTJS-TESTING/base-concert-venue/__tests__/ui/band.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import BandComponent from "@/pages/bands/[bandId]";
+
+test("band component displays correct band information", async () => {
+  const { fakeBands } = await readFakeData();
+  render(<BandComponent band={fakeBands[0]} error={null} />);
+
+  // const heading = screen.getByRole("heading", {
+  //   name: /the wandering bunnines/i,
+  // });
+  const heading = screen.getByRole("heading");
+  expect(heading).toBeInTheDocument();
+});
