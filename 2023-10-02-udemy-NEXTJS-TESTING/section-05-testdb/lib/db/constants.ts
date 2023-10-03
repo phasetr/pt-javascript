c68101a9f960a6e6e285cad966712d8b9e4c853f
phasetr <phasetr@gmail.com>
2023-10-04 08:28:46 +0900

    section5-42

---
+++ 2023-10-02-udemy-NEXTJS-TESTING/section-05-testdb/lib/db/constants.ts
@@ -0,0 +1,9 @@
+export const venueCapacity = 400;
+
+// this will eventually use environment variables
+export const getDbPath = (): string => {
+  if (!process.env.DB_PATH){
+    throw new Error('DB_PATH environment variable not set')
+  }
+  return process.env.DB_PATH;
+};
