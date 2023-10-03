8ca4d048ab41cde8b901392f7549863930f7b13e
phasetr <phasetr@gmail.com>
2023-10-03 14:42:59 +0900

    section4-33

---
+++ 2023-10-02-udemy-NEXTJS-TESTING/base-concert-venue/__tests__/__mocks__/msw/handlers.ts
@@ -0,0 +1,13 @@
+import { rest } from "msw";
+
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+
+export const handlers = [
+  rest.get(
+    "http://localhost:3000/api/shows/:showId",
+    async (_req, res, ctx) => {
+      const { fakeShows } = await readFakeData();
+      return res(ctx.json({ show: fakeShows[0] }));
+    }
+  ),
+];
