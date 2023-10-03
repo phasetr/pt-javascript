efc3a0fe2ea2af4cbc0102c71a051a9d612df48f
phasetr <phasetr@gmail.com>
2023-10-03 16:08:19 +0900

    section4-35

--- 2023-10-02-udemy-NEXTJS-TESTING/base-concert-venue/__tests__/__mocks__/msw/handlers.ts
+++ 2023-10-02-udemy-NEXTJS-TESTING/base-concert-venue/__tests__/__mocks__/msw/handlers.ts
@@ -4,13 +4,11 @@ import { readFakeData } from "@/__tests__/__mocks__/fakeData";
 import { fakeUserReservations } from "@/__tests__/__mocks__/fakeData/userReservations";
 
 export const handlers = [
-  rest.get(
-    "http://localhost:3000/api/shows/:showId",
-    async (_req, res, ctx) => {
-      const { fakeShows } = await readFakeData();
-      return res(ctx.json({ show: fakeShows[0] }));
-    }
-  ),
+  rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
+    const { fakeShows } = await readFakeData();
+    const { showId } = req.params;
+    return res(ctx.json({ show: fakeShows[Number(showId as string)] }));
+  }),
   rest.get(
     "http://localhost:3000/api/users/:userId/reservations",
     async (_req, res, ctx) =>
