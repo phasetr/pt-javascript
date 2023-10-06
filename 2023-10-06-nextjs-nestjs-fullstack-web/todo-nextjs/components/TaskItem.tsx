e5abe75b0ed6a2fd90d7e544029fb9c312b8f1d0
phasetr <phasetr@gmail.com>
2023-10-06 22:30:07 +0900

    section2-21

--- 2023-10-06-nextjs-nestjs-fullstack-web/todo-nextjs/components/TaskItem.tsx
+++ 2023-10-06-nextjs-nestjs-fullstack-web/todo-nextjs/components/TaskItem.tsx
@@ -1,17 +1,17 @@
-import { FC } from 'react'
-import { List } from '@mantine/core'
-import { PencilAltIcon, TrashIcon } from '@heroicons/react/solid'
-import { Task } from '@prisma/client'
-import useStore from '../store'
-import { useMutateTask } from '../hooks/useMutateTask'
+import { FC } from "react";
+import { List } from "@mantine/core";
+import { PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
+import { Task } from "@prisma/client";
+import useStore from "../store";
+import { useMutateTask } from "../hooks/useMutateTask";
 
-export const TaskItem: FC<Omit<Task, 'createdAt' | 'updatedAt' | 'userId'>> = ({
+export const TaskItem: FC<Omit<Task, "createdAt" | "updatedAt" | "userId">> = ({
   id,
   title,
   description,
 }) => {
-  const update = useStore((state) => state.updateEditedTask)
-  const { deleteTaskMutation } = useMutateTask()
+  const update = useStore((state) => state.updateEditedTask);
+  const { deleteTaskMutation } = useMutateTask();
   return (
     <List.Item>
       <div className="float-left mr-10">
@@ -22,17 +22,17 @@ export const TaskItem: FC<Omit<Task, 'createdAt' | 'updatedAt' | 'userId'>> = ({
               id,
               title,
               description,
-            })
+            });
           }}
         />
         <TrashIcon
           className="h-5 w-5 cursor-pointer text-blue-500"
           onClick={() => {
-            deleteTaskMutation.mutate(id)
+            deleteTaskMutation.mutate(id);
           }}
         />
       </div>
       <span>{title}</span>
     </List.Item>
-  )
-}
+  );
+};
