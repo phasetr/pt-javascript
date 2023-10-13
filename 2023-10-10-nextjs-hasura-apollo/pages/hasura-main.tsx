8e20fc79639a04a517806f4ba923eb8acca57a35
phasetr <phasetr@gmail.com>
2023-10-13 14:01:49 +0900

    section3-20

---
+++ 2023-10-10-nextjs-hasura-apollo/pages/hasura-main.tsx
@@ -0,0 +1,25 @@
+import { VFC } from "react";
+import { useQuery } from "@apollo/client";
+import { GetUsersQuery } from "../types/generated/graphql";
+import { GET_USERS } from "../queries/queries";
+import { Layout } from "../components/Layout";
+import Link from "next/link";
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS);
+  if (error) {
+    return (
+      <Layout title={"Hasura fetchPolicy"}>
+        <p>Error: {error.message}</p>
+      </Layout>
+    );
+  }
+  return (
+    <Layout title={"Hasura fetchPolicy"}>
+      {data?.users.map((user) => {
+        return <p className={"my-1"} key={user.id}>{user.name}</p>;
+      })}
+      <Link href={"/hasura-sub"}><a className={"mt-6"}>Next</a></Link>
+    </Layout>);
+};
+export default FetchMain;
