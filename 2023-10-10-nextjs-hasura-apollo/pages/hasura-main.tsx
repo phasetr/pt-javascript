7921fb1d3eabf7233a2d0e5216275f564b1a6366
phasetr <phasetr@gmail.com>
2023-10-13 14:11:00 +0900

    section3-21

--- 2023-10-10-nextjs-hasura-apollo/pages/hasura-main.tsx
+++ 2023-10-10-nextjs-hasura-apollo/pages/hasura-main.tsx
@@ -6,7 +6,9 @@ import { Layout } from "../components/Layout";
 import Link from "next/link";
 
 const FetchMain: VFC = () => {
-  const { data, error } = useQuery<GetUsersQuery>(GET_USERS);
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: "network-only"
+  });
   if (error) {
     return (
       <Layout title={"Hasura fetchPolicy"}>
