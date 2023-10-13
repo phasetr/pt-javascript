b8a5dd14595e288edc7c4f492867d7a020a134fc
phasetr <phasetr@gmail.com>
2023-10-13 22:17:03 +0900

    section3-27

---
+++ 2023-10-10-nextjs-hasura-apollo/components/Child.tsx
@@ -0,0 +1,18 @@
+import { memo, VFC } from "react";
+
+interface Props {
+  printMsg: () => void;
+}
+
+export const Child: VFC<Props> = memo(({ printMsg }) => {
+  return <>
+    {console.log("Child rendered")}
+    <p>Child component</p>
+    <button
+      className={"my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"}
+      onClick={printMsg}
+    >
+      click
+    </button>
+  </>;
+});
