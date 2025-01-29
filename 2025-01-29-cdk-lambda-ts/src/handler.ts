2376fc7751b94a31673edc5cba318628cebc19a9
phasetr <phasetr@gmail.com>
2025-01-29 16:01:59 +0900

    commit

--- 2025-01-29-cdk-lambda-ts/src/handler.ts
+++ 2025-01-29-cdk-lambda-ts/src/handler.ts
@@ -1,4 +1,9 @@
-export const handler = async (event: any, context: any) => ({
+import type { APIGatewayProxyEvent, Context } from "aws-lambda";
+
+export const handler = async (
+	event: APIGatewayProxyEvent,
+	context: Context,
+) => ({
 	statusCode: 200,
 	headers: {},
 	body: { message: "Hello World!" },
