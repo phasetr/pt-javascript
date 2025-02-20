730b281eb3cbc47d8cfa0d5f402f8caa036f4f73
phasetr <phasetr@gmail.com>
2025-02-20 16:31:58 +0900

    commit

--- 2025-02-03-cdk-lambda-express-websocket/bin/cdk-lambda-express-websocket.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkLambdaExpressWebsocketStack } from '../lib/cdk-lambda-express-websocket-stack';
-
-const app = new cdk.App();
-new CdkLambdaExpressWebsocketStack(app, 'CdkLambdaExpressWebsocketStack');
