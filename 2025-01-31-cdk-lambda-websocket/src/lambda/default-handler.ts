d66409700bee0ad9e1ba9ed643759607f6812508
phasetr <phasetr@gmail.com>
2025-02-21 10:32:03 +0900

    commit

--- 2025-01-31-cdk-lambda-websocket/src/lambda/default-handler.ts
+++ 2025-01-31-cdk-lambda-websocket/src/lambda/default-handler.ts
@@ -1,29 +1,27 @@
 import type { APIGatewayProxyWebsocketHandlerV2 } from "aws-lambda";
-import * as AWS from "aws-sdk";
+import {
+	ApiGatewayManagementApiClient,
+	PostToConnectionCommand,
+} from "@aws-sdk/client-apigatewaymanagementapi";
 
 export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event) => {
 	try {
-		const callbackAPI = new AWS.ApiGatewayManagementApi({
-			apiVersion: "2018-11-29",
-			endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`,
+		// v3 のクライアントを生成
+		const client = new ApiGatewayManagementApiClient({
+			endpoint: `https://${event.requestContext.domainName}/${event.requestContext.stage}`,
 		});
 
-		const connectionInfo = await callbackAPI
-			.getConnection({
-				ConnectionId: event.requestContext.connectionId,
-			})
-			.promise();
 		const info = {
-			...connectionInfo,
 			connectionId: event.requestContext.connectionId,
 		};
 
-		await callbackAPI
-			.postToConnection({
+		// PostToConnectionCommand を送信
+		await client.send(
+			new PostToConnectionCommand({
 				ConnectionId: event.requestContext.connectionId,
 				Data: `Use the send-message route to send a message. Your info:${JSON.stringify(info)}`,
-			})
-			.promise();
+			}),
+		);
 
 		return {
 			statusCode: 200,
--- 2025-01-31-cdk-lambda-websocket/src/lambda/send-handler.ts
+++ 2025-01-31-cdk-lambda-websocket/src/lambda/send-handler.ts
@@ -1,5 +1,8 @@
 import type { APIGatewayProxyWebsocketHandlerV2 } from "aws-lambda";
-import AWS from "aws-sdk";
+import {
+	ApiGatewayManagementApiClient,
+	PostToConnectionCommand,
+} from "@aws-sdk/client-apigatewaymanagementapi";
 
 export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event) => {
 	try {
@@ -9,22 +12,22 @@ export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event) => {
 		const connectionId = event.requestContext.connectionId;
 		console.log("クライアントの接続ID:", connectionId);
 
-		const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-			apiVersion: "2018-11-29",
-			endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`,
+		// v3 のクライアントを生成
+		const client = new ApiGatewayManagementApiClient({
+			endpoint: `https://${event.requestContext.domainName}/${event.requestContext.stage}`,
 		});
 
 		const response = {
 			message: `FROM AWS: your message is '${message}'`,
 		};
 
-		// 指定されたconnectionIdへメッセージを投稿
-		await apigwManagementApi
-			.postToConnection({
+		// 指定された connectionId へメッセージを投稿
+		await client.send(
+			new PostToConnectionCommand({
 				ConnectionId: connectionId,
 				Data: JSON.stringify(response),
-			})
-			.promise();
+			}),
+		);
 
 		return {
 			statusCode: 200,
