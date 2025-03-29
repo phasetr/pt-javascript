7acb0f4e01736b1e0bf85091849d2f29f0965a5a
phasetr <phasetr@gmail.com>
2025-03-29 22:18:40 +0900

    commit

--- 2025-03-28-cdk-basic-auth-lambda/packages/db/src/client.ts
+++ 2025-03-28-cdk-basic-auth-lambda/packages/db/src/client.ts
@@ -11,86 +11,88 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 // ローカルにアクセスするためだけのダミーアクセスキーを設定。
 // 何も権限情報も存在しない。
 const devConfig = {
-	endpoint: "http://localhost:8000",
-	region: "ap-northeast-1",
-	credentials: {
-		accessKeyId: "dummy",
-		secretAccessKey: "dummy",
-	},
+  endpoint: "http://localhost:8000",
+  region: "ap-northeast-1",
+  credentials: {
+    accessKeyId: "dummy",
+    secretAccessKey: "dummy"
+  }
 };
 
+// Create the DynamoDB client
 const client = new DynamoDBClient(
-	process.env.ENV === "local" ? devConfig : {},
+  process.env.ENV === "local" ? devConfig : {}
 );
 
+// Create the DynamoDB document client
 const docClient = DynamoDBDocumentClient.from(client);
 
 // 環境に応じたテーブル名を取得
 const getTableName = () => {
-	const prefix = "CBAL";
-	const env = process.env.ENV || "local";
-	return `${prefix}-${env}Todos`;
+  const prefix = "CBAL";
+  const env = process.env.ENV || "local";
+  return `${prefix}-${env}Todos`;
 };
 
 // 環境に応じたインデックス名を取得
 const getIndexName = () => {
-	const prefix = "CBAL";
-	const env = process.env.ENV || "local";
-	return `${prefix}-${env}UserIdIndex`;
+  const prefix = "CBAL";
+  const env = process.env.ENV || "local";
+  return `${prefix}-${env}UserIdIndex`;
 };
 
 // テーブルとGSIを作成
 const createTodosTable = async () => {
-	const params = {
-		TableName: getTableName(),
-		KeySchema: [{ AttributeName: "id", KeyType: KeyType.HASH }],
-		AttributeDefinitions: [
-			{ AttributeName: "id", AttributeType: ScalarAttributeType.S },
-			{ AttributeName: "userId", AttributeType: ScalarAttributeType.S },
-		],
-		ProvisionedThroughput: {
-			ReadCapacityUnits: 5,
-			WriteCapacityUnits: 5,
-		},
-		GlobalSecondaryIndexes: [
-			{
-				IndexName: getIndexName(), // 環境に応じたインデックス名を使用
-				KeySchema: [{ AttributeName: "userId", KeyType: KeyType.HASH }],
-				Projection: { ProjectionType: ProjectionType.ALL },
-				ProvisionedThroughput: {
-					ReadCapacityUnits: 5,
-					WriteCapacityUnits: 5,
-				},
-			},
-		],
-	};
+  const params = {
+    TableName: getTableName(),
+    KeySchema: [{ AttributeName: "id", KeyType: KeyType.HASH }],
+    AttributeDefinitions: [
+      { AttributeName: "id", AttributeType: ScalarAttributeType.S },
+      { AttributeName: "userId", AttributeType: ScalarAttributeType.S }
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5
+    },
+    GlobalSecondaryIndexes: [
+      {
+        IndexName: getIndexName(), // 環境に応じたインデックス名を使用
+        KeySchema: [{ AttributeName: "userId", KeyType: KeyType.HASH }],
+        Projection: { ProjectionType: ProjectionType.ALL },
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 5,
+          WriteCapacityUnits: 5
+        }
+      }
+    ]
+  };
 
-	try {
-		await client.send(new CreateTableCommand(params));
-		console.log("Todos table created successfully with UserIdIndex");
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    await client.send(new CreateTableCommand(params));
+    console.log("Todos table created successfully with UserIdIndex");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // ローカルデータベース内にテーブル作成
 const initializeDynamoDB = async () => {
-	if (process.env.ENV === "local") {
-		try {
-			const tableName = getTableName();
-			const { TableNames } = await client.send(new ListTablesCommand({}));
-			if (TableNames && !TableNames.includes(tableName)) {
-				await createTodosTable();
-			} else if (TableNames) {
-				console.log(`${tableName} table already exists`);
-			} else {
-				console.log(`Unable to list tables, creating ${tableName} table`);
-				await createTodosTable();
-			}
-		} catch (err) {
-			console.error("Error initializing DynamoDB:", err);
-		}
-	}
+  if (process.env.ENV === "local") {
+    try {
+      const tableName = getTableName();
+      const { TableNames } = await client.send(new ListTablesCommand({}));
+      if (TableNames && !TableNames.includes(tableName)) {
+        await createTodosTable();
+      } else if (TableNames) {
+        console.log(`${tableName} table already exists`);
+      } else {
+        console.log(`Unable to list tables, creating ${tableName} table`);
+        await createTodosTable();
+      }
+    } catch (err) {
+      console.error("Error initializing DynamoDB:", err);
+    }
+  }
 };
 
 // テーブル初期化を実行
