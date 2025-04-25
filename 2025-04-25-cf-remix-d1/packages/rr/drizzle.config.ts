eedff723969daa71d7a1f1010903dea94008bb2d
phasetr <phasetr@gmail.com>
2025-04-25 18:36:32 +0900

    commit

--- 2025-04-25-cf-remix-d1/packages/rr/drizzle.config.ts
+++ 2025-04-25-cf-remix-d1/packages/rr/drizzle.config.ts
@@ -6,7 +6,8 @@ export default {
   dialect: 'sqlite',
   // SQLiteとして設定（D1はSQLiteと互換性がある）
   dbCredentials: {
-    url: ':memory:' // 実際のマイグレーション実行時はD1を使用
+    // ローカル開発時はWranglerが作成するローカルのD1データベースファイルを使用
+    url: './.wrangler/state/v3/d1/miniflare-D1DatabaseObject/f253421848505bfd644490698e36d17977501ad2587c6ba0fd479180a316f09a.sqlite'
   },
   verbose: true,
   strict: true,
